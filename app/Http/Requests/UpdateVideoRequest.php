0e5b5118a3f73e3712237c6094178fe6524280fb
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-04-05 01:29:08 +0330

    Video Update Form Request
    UpdateVideoRequest extends the StoreVideoRequest and merge the required rule in update process

--- app/Http/Requests/UpdateVideoRequest.php
+++ app/Http/Requests/UpdateVideoRequest.php
@@ -3,17 +3,11 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
-class UpdateVideoRequest extends FormRequest
+class UpdateVideoRequest extends StoreVideoRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -21,8 +15,8 @@ class UpdateVideoRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
-        ];
+        return array_merge(parent::rules(), [
+            'slug' => ['required', Rule::unique('videos')->ignore($this->video), 'alpha_dash'],
+        ]);
     }
 }
