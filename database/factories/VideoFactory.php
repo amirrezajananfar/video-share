a474a21b542ea77c2c3643d54fa58094c2da7cfd
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-04-05 19:38:53 +0330

    Video Factory Updated

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,7 +23,8 @@ class VideoFactory extends Factory
             'length' => $this->faker->randomNumber(3),
             'url' => 'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4',
             'description' => $this->faker->realText(),
-            'thumbnail' => 'https://wallpaperswide.com/download/mountain_lake_landscape_nature-wallpaper-1920x1080.jpg'
+            'thumbnail' => 'https://wallpaperswide.com/download/mountain_lake_landscape_nature-wallpaper-1920x1080.jpg',
+            'category_id' => Category::inRandomOrder()->first() ?? Category::factory()
         ];
     }
 }
