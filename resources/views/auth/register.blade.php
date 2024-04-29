c5fa77d81553166f3cbb7fb73dbe7cb1a37588a3
amirrezajananfar <amirrezajananfar@gmail.com>
2024-04-29 13:06:49 +0330

    Login & Register Pages Personalized

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,42 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<x-auth-layout>
+    <x-slot name="content">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <body class="sing-up-page">
+            <div id="log-in" class="site-form log-in-form">
+                <div id="log-in-head">
+                    <h1>ثبت نام</h1>
+                    <div id="logo">
+                        <a href="{{ url('/') }}">
+                            <img src="{{ Vite::asset('resources/images/logo.png') }}" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="form-output">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="form-group label-floating">
+                            <label class="control-label" for="name">نام و نام خانوادگی</label>
+                            <input id="name" class="form-control" name="name" value="{{ old('name') }}" required autofocus autocomplete="name">
+                        </div>
+                        <div class="form-group label-floating">
+                            <label class="control-label" for="email">ایمیل</label>
+                            <input id="email" class="form-control" type="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        </div>
+                        <div class="form-group label-floating">
+                            <label class="control-label" for="password">رمز عبور</label>
+                            <input id="password" class="form-control" type="password" name="password" required autocomplete="new-password">
+                        </div>
+                        <div class="form-group label-floating">
+                            <label class="control-label" for="password_confirmation">تأیید رمز عبور</label>
+                            <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                        <input type="submit" class="btn btn-lg btn-primary full-width" value="ثبت نام">
+                        <x-validation-error />
+                        <div class="or"></div>
+                        <p>آیا یک حساب کاربری دارید؟ <a href="{{ route('login') }}"> ورود!</a> </p>
+                    </form>
+                </div>
+            </div>
+        </body>
+    </x-slot>
+</x-auth-layout>
---
+++ resources/views/components/auth-layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video Share</title>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Raleway:400,500,700|Roboto:300,400,500,700,900|Ubuntu:300,300i,400,400i,500,500i,700" rel="stylesheet">
+    @vite(['resources/css/bootstrap.css', 'resources/css/styles.css', 'resources/css/responsive.css', 'resources/js/scripts.js'])
+</head>
+
+{{ $content ?? '' }}
+
+</html>
